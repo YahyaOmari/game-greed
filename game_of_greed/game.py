4a31adf8760726ba2e56a853a0443f7b43cd9a42
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-05-16 14:44:48 +0300

    calculate score

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,2 +1,40 @@
+from collections import Counter
+
 class GameLogic:
-    pass
+    @staticmethod
+    def calculate_score(dice_roll):
+        score=0
+        dice_counter=Counter(dice_roll)
+        for key in dice_counter:
+            count = dice_counter[key]
+            if count < 3:
+                if key == 1:
+                    score += 100
+                elif key == 5:
+                    score += 50
+            else:
+                if count >= 3:
+                    score += key * 100
+                    if key == 1:
+                        score = 1000
+                if count >= 4:
+                    if key == 1:
+                        score += 1000
+                    else:
+                        score += key * 100
+                if count >= 5:
+                    if key == 1:
+                        score += 1000
+                    else:
+                        score += key * 100
+
+                if count == 6:
+                    if key == 1:
+                        score += 1000
+                    else:
+                        score += key * 100
+
+        if len(dice_counter) == 6:
+            score = 1500
+
+            
