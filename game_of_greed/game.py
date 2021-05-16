fd819fcb6752c7aceccc96e42fe692707335ccd4
RaneemAbdulazez <raneem97su@gmail.com>
2021-05-16 14:19:19 +0300

    banker done

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,2 +1,23 @@
+"""Define a GameLogic class in game_of_greed/game_logic.py file.
+Handle calculating score for dice roll
+Add calculate_score static method to GameLogic class.
+The input to calculate_score is a tuple of integers that represent a dice roll.
+The output from calculate_score is an integer representing the roll’s score according to rules of game.
+
+"""
+
+
 class GameLogic:
     pass
+
+
+# 6 >{3,3,3,3,3}
+
+
+"""Handle rolling dice
+Add roll_dice static method to GameLogic class.
+The input to roll_dice is an integer between 1 and 6.
+The output of roll_dice is a tuple with random values between 1 and 6.
+The length of tuple must match the argument given to roll_dice method."""
+
+
